8a5428ddb11cec63f45dc8eaa460465fca56bb9f
kevinCSDN <81788909+kevinCSDN@users.noreply.github.com>
2024-01-09 18:38:56 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,14 +11,36 @@
 #
 
 # Uncomment a feed source
-#sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '$a src-git small1 https://github.com/kenzok8/small' feeds.conf.default
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
-# Add a feed source
- #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
- #echo 'src-git sundaqiang https://github.com/sundaqiang/openwrt-packages-backup' >>feeds.conf.default
- #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+#删除feeds自带mosdns、v2ray-geodata
+rm -rf ./feeds/packages/net/mosdns
+rm -rf feeds/packages/net/v2ray-geodata
+rm -rf feeds/luci/themes/luci-theme-argon
 
 
+
+
+
+
+
+
+#克隆passwall环境插件
+git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/pwpage
+
+#克隆的源码放在small文件夹,预先建立small文件夹
+mkdir package/small
+pushd package/small
+
+#克隆源码
+
+#passwall2
+git clone -b main --depth 1 https://github.com/xiaorouji/openwrt-passwall2.git
+git clone -b main --depth 1 https://github.com/xiaorouji/openwrt-passwall.git
+#mosdns
+#git clone -b v5 --depth 1 https://github.com/sbwml/luci-app-mosdns.git
+git clone --depth=1 https://github.com/fw876/helloworld.git
+#git clone https://github.com/kevinCSDN/lua-maxminddb.git  #git lua-maxminddb 依赖
+
+#git clone https://github.com/kevinCSDN/luci-app-vssr-plus.git  
+
+popd
